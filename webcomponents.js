64d2f8833639f55d8217483d08f48d3b09d94005
Rorronarvaez1 <narvaezrodrigo2022@gmail.com>
2026-02-19 16:18:53 -0600

    Web comonents for header and footer

---
+++ webcomponents.js
@@ -0,0 +1,33 @@
+class SiteHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <header class="site-header">
+      <div class="dash-menu">
+        <h1>Menu</h1>
+        <h3>Navigation</h3>
+        <ul>
+          <li><a href="dashboard.html">Home</a></li>
+          <li><a href="projects.html">Projects</a></li>
+          <li><a href="calendar.html">Calendar</a></li>
+          <li><a href="#">Settings</a></li>
+        </ul>
+      </div>
+    </header>
+    `;
+  }
+}
+
+class SiteFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <footer class="site-footer">
+      <div class="container">
+        <p>&copy; 2026 Project Manager. All rights reserved.</p>
+      </div>
+    </footer>
+    `;
+  }
+}
+
+customElements.define('site-header', SiteHeader);
+customElements.define('site-footer', SiteFooter);
